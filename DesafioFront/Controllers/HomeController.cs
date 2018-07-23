a0dfaae6a8f7dba074a30a583af95d24dceed2dc
danielsantosc19 <41124762+danielsantosc19@users.noreply.github.com>
2018-07-23 00:14:26 -0300

    Add files via upload

---
+++ DesafioFront/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DesafioFront.Models;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Runtime.Serialization.Json;
+
+namespace DesafioFront.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index2()
+        {
+            var factory = new ConnectionFactory() { HostName = "35.199.98.99", Port = 5672 };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "rpc_queue",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);                                        
+
+                    System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                    Models.Operacao operacao = new Operacao() { Id = 1, Side = "buy", Price = 19, Quantity = 10, Symbol = "PETR4" };
+
+                    DataContractJsonSerializer s = new DataContractJsonSerializer(typeof(Operacao));
+                    s.WriteObject(ms, operacao);
+                    byte[] json = ms.ToArray();
+                    ms.Close();
+                    string body = Encoding.UTF8.GetString(json, 0, json.Length);
+
+                    channel.BasicPublish(exchange: "", 
+                        routingKey: "rpc_queue", 
+                        basicProperties: null, 
+                        body: json
+                        );
+                    
+                }
+            }
+            return View();
+        }
+
+        
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DesafioFront/Models/Operacao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioFront.Models
+{
+    public class Operacao
+    {
+        public int Id { get; set; }
+
+        public string Side { get; set; }
+
+        public Decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Symbol { get; set; }
+    }
+}
